62bbc030f2e90069627dcb18652b4ca88e1d829a
jeff5m <jeffmatheuspereira@gmail.com>
2021-06-02 15:30:58 -0300

    create Prato CRUD

--- cadastro/src/main/java/com/github/jeff5m/ifood/cadastro/Prato.java
+++ cadastro/src/main/java/com/github/jeff5m/ifood/cadastro/Prato.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 public class Prato extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    public Long id;
     @NotBlank
     public String nome;
     @NotBlank
