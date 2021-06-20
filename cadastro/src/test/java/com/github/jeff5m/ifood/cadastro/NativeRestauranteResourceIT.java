a77bce8a42453e2899dfafd4ffab7b3640069506
jeff5m <jeffmatheuspereira@gmail.com>
2021-06-20 18:45:51 -0300

    delete unused native test

--- cadastro/src/test/java/com/github/jeff5m/ifood/cadastro/NativeRestauranteResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.github.jeff5m.ifood.cadastro;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeRestauranteResourceIT extends RestauranteResourceTest {
-
-    // Execute the same tests but in native mode.
-}
